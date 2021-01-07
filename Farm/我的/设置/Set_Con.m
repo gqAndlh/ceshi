e6928fb67ac56f757cfc9e23c26a301dc29a0feb
gqAndlh <1020338145@qq.com>
2021-01-07 12:30:04 +0800

    在此修改

--- Farm/我的/设置/Set_Con.m
+++ Farm/我的/设置/Set_Con.m
@@ -18,7 +18,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.title=@"用户";
+    self.title=@"用户设置";
     // Do any additional setup after loading the view from its nib.
 }
 - (IBAction)menuClick:(UIButton *)sender {
